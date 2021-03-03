37bcfa8b96c35a5086777cbe80f9f250b891bc5b
AndresJimenezg <79484192+AndresJimenezg@users.noreply.github.com>
2021-03-02 21:08:38 -0600

    Add files via upload

---
+++ Actividad_1.cpp
@@ -0,0 +1,10 @@
+/*ACTIVIDAD 1 DE PROGRAMACION ORIENTADA A OBJETOS
+*/
+#include <iostream>
+ using namespace std;
+ 
+int main()
+{
+	cout<<"Hola mundo\n" << endl;
+	cout<<"Primer tarea del curso de Programacion Orientada a objetos\n" << endl;
+}
